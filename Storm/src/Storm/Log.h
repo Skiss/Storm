06468a354548bcd9d7eb8c09691df7833fd59ee9
Jeremy Chanut <jeremy.chanut@gmail.com>
2022-08-23 21:51:38 -0400

    Logging only for Debug and Release

--- Storm/src/Storm/Log.h
+++ Storm/src/Storm/Log.h
@@ -24,9 +24,18 @@ private:
 
 }
 
+#ifdef STORM_DIST
+#define ST_LOG_TRACE(...) do {} while (false)
+#define ST_LOG_DEBUG(...) do {} while (false)
+#define ST_LOG_INFO(...) do {} while (false)
+#define ST_LOG_WARNING(...) do {} while (false)
+#define ST_LOG_ERROR(...) do {} while (false)
+#define ST_LOG_CRITICAL(...) do {} while (false)
+#else
 #define ST_LOG_TRACE(...) storm::Log::getLogger()->trace(__VA_ARGS__)
 #define ST_LOG_DEBUG(...) storm::Log::getLogger()->debug(__VA_ARGS__)
 #define ST_LOG_INFO(...) storm::Log::getLogger()->info(__VA_ARGS__)
 #define ST_LOG_WARNING(...) storm::Log::getLogger()->warn(__VA_ARGS__)
 #define ST_LOG_ERROR(...) storm::Log::getLogger()->error(__VA_ARGS__)
 #define ST_LOG_CRITICAL(...) storm::Log::getLogger()->critical(__VA_ARGS__)
+#endif
